563f48fdf18167d668504378c5acee00059bb84e
Thailan Godoy <thailanrg@gmail.com>
2025-07-05 09:47:46 -0300

    additing modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,10 @@ dependencyResolutionManagement {
 
 rootProject.name = "advanced modularization api impl"
 include(":app")
+include(":feature:feature-a")
+include(":feature:feature-b")
+include(":core:kotlin")
+include(":core:navigation")
+include(":core:network")
+include(":core:android")
+include(":core:commons")
